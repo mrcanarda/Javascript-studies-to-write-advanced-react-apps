b78592eea4ea4e527e616a7b25ae69cadc1ee12f
Can Arda <canarda.dev@gmail.com>
2024-10-23 17:34:40 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -324,3 +324,34 @@ const booksAfterUpdate = booksAfterDelete.map((book) =>
 );
 
 booksAfterUpdate;
+
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+
+// console.log("Can");
+
+// 1
+
+// async function getTodos() {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+//   const data = await res.json();
+//   console.log(data);
+// }
+
+// getTodos();
+
+// 2
+
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(data);
+
+  return data;
+}
+
+const todos = getTodos();
+console.log(todos);
+
+console.log("Can");
